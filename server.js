1a8d34c1b7a60817cd0d9dea90bb5b30220d92c7
Siddharth Ajmera <Siddharth.Ajmera@T-Mobile.com>
2017-11-23 01:17:52 +0530

    Made changes for HEroku Deployment

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-var app = express();
+const port = process.env.PORT || 3000;
+const app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -14,9 +15,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.render('maintenance.hbs');
-});
+}); */
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
@@ -45,4 +46,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is Up on PORT ${port}`);
+});
